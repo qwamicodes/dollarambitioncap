419d4b5337277ac9287377c24a78a45512502d0c
kwaame-adjekum <kwaame.oforiadjekum@rancard.com>
2023-12-11 11:58:19 +0000

    mobile nav

--- src/layout/navbar.tsx
+++ src/layout/navbar.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import Logo from "../assets/images/logo.png";
 
 const Navbar = () => {
+  const [showNav, setShowNav] = React.useState(false);
+  const { pathname } = useLocation();
+
+  const liClassname = "border-b border-gray-200 px-5 py-4";
   return (
     <nav className="max-w-[1440px] min-[320px]:w-full flex items-center justify-center w-full mx-auto h-[7rem] fixed z-20 bg-white/7 text-white backdrop-blur-sm shadow-sm shadow-black/8">
       <div className="w-3/4 flex items-center justify-between">
@@ -28,24 +32,46 @@ const Navbar = () => {
             <span>contact</span>
           </li>
         </ul>
-        <ul className="hidden flex flex-col bg-white text-black py-5 gap-5 uppercase font-medium justify-between items-center basis-[40%] text-3xl">
-          <li>
-            <Link to="/">home</Link>
+        <div className="w-[4rem] rounded-md p-3 bg-[#0AA92F]">
+          <span onClick={() => setShowNav(!showNav)}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="ionicon"
+              viewBox="0 0 512 512"
+            >
+              <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeMiterlimit={10}
+                strokeWidth={32}
+                d="M80 160h352M80 256h352M80 352h352"
+              />
+            </svg>
+          </span>
+        </div>
+      </div>
+      {showNav && (
+        <ul className="absolute top-full w-[100vw] flex-col bg-white text-gray-500  gap-5 uppercase font-medium justify-between items-center basis-[40%] text-2xl">
+          <li className={liClassname}>
+            <Link to="/" className={pathname === "/" ? "text-red-400" : ""}>
+              home
+            </Link>
           </li>
-          <li>
+          <li className={liClassname}>
             <span>blog</span>
           </li>
-          <li>
+          <li className={liClassname}>
             <span>services</span>
           </li>
-          <li>
+          <li className={liClassname}>
             <span>about</span>
           </li>
-          <li>
+          <li className={liClassname}>
             <span>contact</span>
           </li>
         </ul>
-      </div>
+      )}
     </nav>
   );
 };
